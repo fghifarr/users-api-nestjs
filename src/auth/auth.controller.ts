0665b8dd2b8fdb183343ce8e6f656c36caecbc5e
F Ghifar R <fghifarr@gmail.com>
2022-01-21 00:21:02 +0700

    Implement Passport JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,42 @@
 import {
   Controller,
+  Get,
   HttpStatus,
   Post,
   Req,
+  Request,
   Res,
   UseGuards,
 } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @ApiTags('auth')
-@Controller('auth')
+@Controller()
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @ApiOkResponse({ description: 'Successfully logged in' })
   @ApiBadRequestResponse({ description: 'Bad Request' })
   @UseGuards(LocalAuthGuard)
-  @Post('login')
+  @Post('auth/login')
   async login(@Req() req, @Res() res) {
-    res.status(HttpStatus.OK).json({
-      message: 'Successfully logged in as: ' + req.user.username,
-    });
+    const tokenObj = await this.authService.login(req.user);
+    return res.status(HttpStatus.OK).json(tokenObj);
+  }
+
+  @ApiOkResponse({ description: 'Successfully get current user' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.userService.findByUsername(username);
@@ -14,5 +18,13 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.id };
+    const accessToken = this.jwtService.sign(payload);
+    return {
+      access_token: accessToken,
+    };
   }
 }

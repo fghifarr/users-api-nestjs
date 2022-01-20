e2de4e33e7e75c9903675161f66fd64d4aef54c0
F Ghifar R <fghifarr@gmail.com>
2022-01-20 12:40:33 +0700

    implement auto-validation

--- src/user/dto/user-create.dto.ts
+++ src/user/dto/user-create.dto.ts
@@ -1,8 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class UserCreateDto {
   @ApiProperty()
+  @IsEmail()
   username: string;
+
   @ApiProperty()
+  @IsNotEmpty()
   password: string;
 }
--- src/user/dto/user-update.dto.ts
+++ src/user/dto/user-update.dto.ts
@@ -1,8 +1,12 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEmail, IsOptional, ValidateIf } from 'class-validator';
 
 export class UserUpdateDto {
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsEmail()
   username: string;
+
   @ApiPropertyOptional()
   password: string;
 }
